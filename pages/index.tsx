2d2bcecc28f835e72c25ce15cbf8f05783f6b3eb
juna4 <muhammad.junaedi@intidata.net>
2022-12-08 18:01:41 +0700

    fetch categories

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Landing from "../components/Landing";
+import { Tab } from "@headlessui/react";
+import { useState } from "react";
+import { GetServerSideProps } from "next";
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    // const categories = await fetchCategories()
+    return {
+        props: {},
+    };
+};
 
 export default function Home() {
+    const [selected, setSelected] = useState(false);
     return (
         <div>
             <Head>
@@ -22,6 +33,22 @@ export default function Home() {
                     <h1 className="text-center text-4xl font-medium tracking-wide text-white md:text-5xl">
                         New Promos
                     </h1>
+                    <Tab.Group>
+                        <Tab.List className="flex justify-center">
+                            <Tab
+                                className={({ selected }) =>
+                                    `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
+                                        selected
+                                            ? "border-gradient bg-[#35383C] text-white"
+                                            : "border-b-2 border-[#35383C] text-[#747474]"
+                                    }`
+                                }
+                            >
+                                Category
+                            </Tab>
+                        </Tab.List>
+                        <Tab.Panels className="mx-auto max-w-fit pt-10 pb-24 sm:px-4"></Tab.Panels>
+                    </Tab.Group>
                 </div>
             </section>
         </div>
