2ec38b5b91d8c8e5ea75aa8414793792733ca763
juna4 <muhammad.junaedi@intidata.net>
2022-12-08 17:42:18 +0700

    fix landing

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,16 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Header />
-            <Landing />
+            <main className="relative h-[200vh]">
+                <Landing />
+            </main>
+            <section className="relative z-40 -mt-[100vh] min-h-screen bg-[#1b1b1b]">
+                <div className="space-y-10 py-16">
+                    <h1 className="text-center text-4xl font-medium tracking-wide text-white md:text-5xl">
+                        New Promos
+                    </h1>
+                </div>
+            </section>
         </div>
     );
 }
